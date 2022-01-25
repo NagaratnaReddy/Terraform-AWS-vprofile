42e1cdbf9abbd8733a064abab6619224bd728099
reddyn6 <nagaratna.reddy@gmail.com>
2022-01-26 01:35:21 +0530

    Adding VPC

--- variable.tf
+++ variable.tf
@@ -1,96 +1,96 @@
 variable "REGION" {
-type = string
-default = "ap-south-1"
-description= "mumbai region"
+  type        = string
+  default     = "ap-south-1"
+  description = "mumbai region"
 }
 
 variable "AMIS" {
-type = map(string)
-default = {
-   ap-south-1 = "ami-0851b76e8b1bce90b"
-   us-east-2 = "ami-0fb653ca2d3203ac1"
-}
+  type = map(string)
+  default = {
+    ap-south-1 = "ami-0851b76e8b1bce90b"
+    us-east-2  = "ami-0fb653ca2d3203ac1"
+  }
 }
 
 variable "PRIVATE_KEY_PATH" {
-  default ="vprofilekey"
+  default = "vprofilekey"
 }
 
 variable "PUB_KEY_PATH" {
- default ="vprofilekey.pub"
+  default = "vprofilekey.pub"
 }
 
 variable "USERNAME" {
- default ="ubuntu"
+  default = "ubuntu"
 }
 
 variable "rmquser" {
-default = "rabbit"
+  default = "rabbit"
 }
 
 variable "rmqpass" {
-default = "Password@123"
+  default = "Password@123"
 }
 
 variable "dbuser" {
-default = "admin"
+  default = "admin"
 }
 
 variable "dbpass" {
-default = "admin123"
+  default = "admin123"
 }
 
 variable "dbname" {
-default = "accounts"
+  default = "accounts"
 }
 
 variable "instance_count" {
-default = 1
-type = number
+  default = 1
+  type    = number
 }
 
 variable "VPC_NAME" {
-default = "vprofile-VPC"
+  default = "vprofile-VPC"
 }
 
 variable "Zone1" {
-default = "ap-south-1a"
+  default = "ap-south-1a"
 }
 
 variable "Zone2" {
-default = "ap-south-1b"
+  default = "ap-south-1b"
 }
 
 variable "Zone3" {
-default = "ap-south-1c"
+  default = "ap-south-1c"
 }
 
 variable "VPC-CIDR" {
-default = "172.21.0.0/16"
+  default = "172.21.0.0/16"
 }
 
 variable "PubSub1-CIDR" {
-default = "172.21.1.0/24"
+  default = "172.21.1.0/24"
 }
 
 variable "PubSub2-CIDR" {
-default = "172.21.2.0/24"
+  default = "172.21.2.0/24"
 }
 
 variable "PubSub3-CIDR" {
-default = "172.21.3.0/24"
+  default = "172.21.3.0/24"
 }
 
 variable "PrivSub1-CIDR" {
-default = "172.21.4.0/24"
+  default = "172.21.4.0/24"
 }
 
 variable "PrivSub2-CIDR" {
-default = "172.21.5.0/24"
+  default = "172.21.5.0/24"
 }
 
 variable "PrivSub3-CIDR" {
-default = "172.21.6.0/24"
+  default = "172.21.6.0/24"
 }
 
 
--- vpc.tf
+++ vpc.tf
@@ -1,20 +1,19 @@
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  name = var.VPC_NAME
-  cidr = var.VPC-CIDR
+  source = "terraform-aws-modules/vpc/aws"
+  name   = var.VPC_NAME
+  cidr   = var.VPC-CIDR
+  azs             = [var.Zone1, var.Zone2, var.Zone3]
+  private_subnets = [var.PrivSub1-CIDR, var.PrivSub2-CIDR, var.PrivSub3-CIDR]
+  public_subnets  = [var.PubSub1-CIDR, var.PubSub2-CIDR, var.PubSub3-CIDR]
 
-  azs                 = [var.Zone1, var.Zone2, var.Zone3]
-  private_subnets     = [var.PrivSub1-CIDR, var.PrivSub2-CIDR, var.PrivSub3-CIDR]
-  public_subnets      = [var.PubSub1-CIDR, var.PubSub3-CIDR, var.PubSub3-CIDR]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
-  Terraform ="true"
-  Environment = "Test"}
+    Terraform = "true"
+  Environment = "Test" }
 
   vpc_tags = {
     Name = var.VPC_NAME
